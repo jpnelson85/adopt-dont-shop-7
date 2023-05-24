abde9ec4a0b09505645df2796a8a0a5f3275d681
Jimmy Jurado <jurado140@gmail.com>
2023-05-24 12:58:13 -0400

    Completes US13.

--- spec/features/admin/applications/show_index.rb
+++ spec/features/admin/applications/show_index.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.describe "Admin Application Show Page" do
-  it "will have button to approve application for each pet" do
+  it "Approving a Pet for Adoption" do
     shelter_1 = Shelter.create(name: "Aurora shelter", city: "Aurora, CO", foster_program: false, rank: 9)
     pet_1 = Pet.create(adoptable: true, age: 1, breed: "sphynx", name: "Bare-y Manilow", shelter_id: shelter_1.id)
     applicant_1 = Applicant.create!(name: "Jimmy", street_address: "1234 road test", city: "Boca Raton", state: "FL", zip_code: "33498", qualification: "I love pets", application_status: "Pending")
@@ -18,6 +18,23 @@ RSpec.describe "Admin Application Show Page" do
 
     expect(page).to_not have_button("Approve Application")
     expect(page).to have_content("Approved")
+  end
+
+  it "Rejecting a Pet for Adoption" do
+    shelter_1 = Shelter.create(name: "Aurora shelter", city: "Aurora, CO", foster_program: false, rank: 9)
+    pet_1 = Pet.create(adoptable: true, age: 1, breed: "sphynx", name: "Bare-y Manilow", shelter_id: shelter_1.id)
+    applicant_1 = Applicant.create!(name: "Jimmy", street_address: "1234 road test", city: "Boca Raton", state: "FL", zip_code: "33498", qualification: "I love pets", application_status: "Pending")
+    app_1 = ApplicantPet.create!(applicant: applicant_1, pet: pet_1)
+
+    visit "/admin/applications/#{applicant_1.id}"
+
+    expect(page).to have_button("Reject Application")
+    click_button("Reject Application")
+save_and_open_page
+    expect(page).to_not have_button("Approve Application")
+    expect(page).to_not have_button("Reject Application")
+
+    expect(page).to have_content("Rejected")
 
   end
 end
