443206595ea66428fd95bb2e52b82bce97e20bce
Jeff Nelson <jpnelson85@gmail.com>
2023-05-24 17:13:20 -0500

    create more robust testing spec/features/applicants/show_spec

--- spec/features/applicants/show_spec.rb
+++ spec/features/applicants/show_spec.rb
@@ -1,27 +1,11 @@
 require "rails_helper"
 
 RSpec.describe "the application show page" do
-
-#   1. Application Show Page
-
-# As a visitor
-# When I visit an applications show page
-# Then I can see the following:
-# - Name of the Applicant
-# - Full Address of the Applicant including street address, city, state, and zip code
-# - Description of why the applicant says they'd be a good home for this pet(s)
-# - names of all pets that this application is for (all names of pets should be links to their show page)
-# - The Application's status, either "In Progress", "Pending", "Accepted", or "Rejected"
-
-
   it "shows an application show page with form attributes" do
     shelter = Shelter.create!(name: "Aurora shelter", city: "Aurora, CO", foster_program: false, rank: 9)
-    
     pet_1 = shelter.pets.create!(name: "Scooby", age: 2, breed: "Great Dane", adoptable: true, shelter_id: shelter.id)
-    # pet_2 = pet = Pet.create(name: "Scrappy", age: 1, breed: "Bull Dog", adoptable: true, shelter_id: shelter.id)
-
     applicant_1 = Applicant.create!(name: "Jimmy", street_address: "1234 road test", city: "Boca Raton", state: "FL", zip_code: "33498", qualification: "I love pets", application_status: "In Progress")
-
+    applicant_2 = Applicant.create!(name: "Bob", street_address: "568 Main Street", city: "Springfield", state: "GA", zip_code: "56898", qualification: "because", application_status: "In Progress")
     app_1 = ApplicantPet.create!(applicant: applicant_1, pet: pet_1)
 
     visit "applicants/#{applicant_1.id}"
@@ -32,6 +16,9 @@ RSpec.describe "the application show page" do
     expect(page).to have_content("#{pet_1.name}")
     expect(page).to have_content(applicant_1.application_status)
     expect(page).to have_link("Scooby")
+    expect(page).to_not have_content(applicant_2.name)
+    expect(page).to_not have_content("Full Address: #{applicant_2.street_address} #{applicant_2.city} #{applicant_2.state} #{applicant_2.zip_code}")
+    expect(page).to_not have_content(applicant_2.qualification)
   end
 
   it "lists matches as search results" do
@@ -48,7 +35,6 @@ RSpec.describe "the application show page" do
     click_on("Search")
 
     expect(current_path).to eq("/applicants/#{applicant_1.id}")
-
 
     expect(page).to have_content(pet_1.name)
     expect(page).to_not have_content(pet_2.name)
@@ -72,7 +58,6 @@ RSpec.describe "the application show page" do
 
     click_on("Adopt this Pet")
 
-
     expect(current_path).to eq("/applicants/#{applicant_1.id}")
     expect(page).to have_content("#{pet_1.name}")
   end
@@ -85,10 +70,14 @@ RSpec.describe "the application show page" do
     applicant_1 = Applicant.create!(name: "Jimmy", street_address: "1234 road test", city: "Boca Raton", state: "FL", zip_code: "33498", qualification: "I love pets")
     app_1 = ApplicantPet.create!(applicant: applicant_1, pet: pet_1)
     app_2 = ApplicantPet.create!(applicant: applicant_1, pet: pet_2)
+
     visit "/applicants/#{applicant_1.id}"
+    
     expect(page).to have_button("Submit Application")
+    
     fill_in(:qualification, with: "I have land and need for more pets")
     click_button("Submit Application")
+    
     expect(page).to have_content("Reason: I have land and need for more pets")
     expect(page).to have_content("App Status: Pending")
     expect(page).to_not have_content("In Progress")
